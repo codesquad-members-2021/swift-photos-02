aea884194b8df1cd9f268d558e25ffad5e4bcf8f
Jisoo Kim <ghis22130@naver.com>
2021-03-22 16:44:03 +0900

    포토 컬렉션 뷰 오토리사이징 적용

---
+++ PhotoLibraryApp/PhotoLibraryApp/PhotoCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCollectionView.swift
+//  PhotoLibraryApp
+//
+//  Created by 지북 on 2021/03/22.
+//
+
+import UIKit
+
+class PhotoCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
