91ff954692863d4210417c0f71162a7542c9c787
Jisoo Kim <ghis22130@naver.com>
2021-03-23 11:18:10 +0900

    ViewController에서 CollectionView 분리, CollectionCell Size 100x

--- PhotoLibraryApp/PhotoLibraryApp/Model/ColorGenerator.swift
+++ PhotoLibraryApp/PhotoLibraryApp/Model/ColorGenerator.swift
@@ -10,12 +10,10 @@ import Foundation
 import UIKit
 
 struct ColorGenerator {
-    
     static func getRandomColor() -> UIColor {
         let randomRed: CGFloat = CGFloat(drand48())
         let randomGreen: CGFloat = CGFloat(drand48())
         let randomBlue: CGFloat = CGFloat(drand48())
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
-
 }
--- PhotoLibraryApp/PhotoLibraryApp/ViewController.swift
+++ PhotoLibraryApp/PhotoLibraryApp/ViewController.swift
@@ -9,35 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var photoCollectionView: PhotoCollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
-    }
-    
-    func configureCollectionView() {
-        photoCollectionView.dataSource = self
-        photoCollectionView.delegate = self
     }
 }
-
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else {
-            return UICollectionViewCell()
-        }
-        return cell
-    }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 80, height: 80)
-    }
-}
-
