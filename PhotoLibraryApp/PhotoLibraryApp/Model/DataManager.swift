2b4fe34e77e5b60eedf5a8e16059f0ad12ed656f
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-24 17:12:57 +0900

    URL로부터 JSON decode를 수행하는 객체 생성

--- PhotoLibraryApp/PhotoLibraryApp/Model/DataManager.swift
+++ PhotoLibraryApp/PhotoLibraryApp/Model/DataManager.swift
@@ -5,4 +5,28 @@
 //  Created by Jun Ho JANG on 2021/03/24.
 //
 
-import Foundation
+import UIKit
+
+class DoodleDataManager {
+    let doodleImageURL = "https://public.codesquad.kr/jk/doodle.json"
+    
+    private(set) var doodleImages = [DoodleImage]()
+    
+    func countImage() -> Int {
+        return doodleImages.count
+    }
+    
+    func decodeJSON() {
+        do {
+            guard let doodleURL = URL(string: doodleImageURL) else {
+                return
+            }
+            guard let JSONData = try String(contentsOf: doodleURL).data(using: .utf8) else {
+                return
+            }
+            self.doodleImages = try JSONDecoder().decode([DoodleImage].self, from: JSONData)
+        } catch {
+            
+        }
+    }
+}
