230b63f7bd6e10c4a044127b51c636e2d1fb6559
Jisoo Kim <ghis22130@naver.com>
2021-03-26 10:05:38 +0900

    plusButton 동작 설정

--- PhotoLibraryApp/PhotoLibraryApp/PhotoAppViewController.swift
+++ PhotoLibraryApp/PhotoLibraryApp/PhotoAppViewController.swift
@@ -11,6 +11,12 @@ import Photos
 class PhotoAppViewController: UIViewController {
     @IBOutlet weak var photoCollectionView: PhotoCollectionView!
     
+    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    @IBAction func plusButtonTapped(_ sender: Any) {
+        self.present(appDelegate.doodleNavigaationController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.title = "Photos"
