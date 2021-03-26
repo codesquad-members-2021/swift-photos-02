a83775d76b0e6af10cf151385b0e517f3c1c49e2
Jisoo Kim <ghis22130@naver.com>
2021-03-26 10:02:36 +0900

    AppDelegate에서 CollectionView 설정

--- PhotoLibraryApp/PhotoLibraryApp/AppDelegate.swift
+++ PhotoLibraryApp/PhotoLibraryApp/AppDelegate.swift
@@ -9,6 +9,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var doodleCollectionViewController: DoodleViewController!
+    var doodleNavigaationController: UINavigationController!
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        doodleCollectionViewController = DoodleViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        doodleNavigaationController = UINavigationController(rootViewController: doodleCollectionViewController)
+        doodleNavigaationController.modalPresentationStyle = .fullScreen
+        return true
+    }
 }
 
